93997c61c6f0223e3253bc2a3a09864bbf440fac
Paul Miranda <paul_mv3@hotmail.com>
2020-12-04 19:14:16 -0800

    added more examples

--- src/components/Lessons/Lesson_2/Conditional/examples.js
+++ src/components/Lessons/Lesson_2/Conditional/examples.js
@@ -1,6 +1,6 @@
 const examples = [
     { example: { first: '一人で住みますから、へやは一つ', highlight: 'あれば、', second: '大丈夫です。' }, translation: 'Porque vivo solo, si solo tiene un cuarto no hay problema.' },
-    { example: { first: '会社の近くで', highlight: '食べれば', second: 'いいから、問題ありません。' }, translation: 'Porque vivo solo, si solo tiene un cuarto no hay problema.' },
-    { example: { first: '週末スーパーでたくさん', highlight: '買えば、', second: '大丈夫です。' }, translation: 'Porque vivo solo, si solo tiene un cuarto no hay problema.' },
+    { example: { first: '会社が近くに住むので、車が', highlight: 'なければ', second: '問題ありません。' }, translation: 'Porque vivo cerca de la empresa, aunque no tenga carro no hay problema.' },
+    { example: { first: '週末に映画で多い人が', highlight: 'あれば、', second: '大丈夫です。' }, translation: 'Aunque el fin de semana este lleno el cine, no hay problema.' },
 ];
 export default examples;
--- src/components/Lessons/Lesson_2/Troubles/troubles_examples.js
+++ src/components/Lessons/Lesson_2/Troubles/troubles_examples.js
@@ -1,4 +1,5 @@
 const examples = [
-    { example: { first: 'いつもは9時にはじまるんです', highlight: 'けど', second: '。' }, tranlation: 'Siempre empieza a las 9... (Situacón real: son mas de las 9 y no han empezado.)' }
+    { example: { first: 'いつもは9時にはじまるんです', highlight: 'けど', second: '。。。' }, translation: 'Siempre empieza a las 9... (Situacón real: son mas de las 9 y no han empezado.)' },
+    { example: { first: '昨日から田中さんに電話をかけてるんです', highlight: 'けど', second: '。。。' }, translation: 'Llevo llamando al señor Tanka desde ayer... (Situacón real: no ha contestado.)' }
 ];
 export default examples;
