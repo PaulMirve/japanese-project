b2169e680bdc65b3a8a4ef7a09a1f227bb24c7f5
Paul Miranda <paul_mv3@hotmail.com>
2020-12-13 12:12:57 -0800

    added lessons to lesson 8

--- src/components/Lessons/Lesson_8/RememberWhatYouWhereDoing/RememberWhatYouWhereDoing.jsx
+++ src/components/Lessons/Lesson_8/RememberWhatYouWhereDoing/RememberWhatYouWhereDoing.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
+import GrammarLesson from '../../../CustomComponents/GrammarLesson';
 import Topic from '../../../CustomComponents/Topic';
+import useLessonStyles from '../../../CustomHooks/lesson_styles';
 import { nameRememberWhatYouWhereDoing } from '../topics_list';
+import examples from './examples';
 
 export default function RememberWhatYouWhereDoing() {
+    const classes = useLessonStyles();
     const Grammar = () => {
-        return <></>
+        return (
+            <>
+                <p className={classes.description}>
+                    A veces hablamos solos cuando intentamos recordar algo que hemos hecho, para hacer esto usamos la siguiente formula:
+                </p>
+                <div className={classes.grammarContainer}>
+                    <GrammarLesson>Verbo en forma て + から、 + Verbo 2</GrammarLesson>
+                </div>
+                <p className={classes.description}>
+                    Cuando queremos recordar en voz alta algo que hemos hecho mencionando el punto de inicio, o bien queremos recorrer con la memoria una secuencia de acciones en el orden adecuado, o incluso deseamos repasar cronológicamente las cosas que hemos ido
+                    haciendo utilizamos la siguiente formula:
+                </p>
+                <div className={classes.grammarContainer}>
+                    <GrammarLesson>Verbo en forma diccionario pasado + あとで、 Verbo 2</GrammarLesson>
+                </div>
+            </>
+        );
     }
-    const description = '';
-    return <Topic name={nameRememberWhatYouWhereDoing} grammar={Grammar} description={description} />
+    const description = 'Se trata de diferenciar el uso del estilo formal y el estilo informal (ver L4 y L5). En esta lección, distinguimos entre el estilo informal, que utilizamos cuando nos decimos a nosotros mismos lo que pensamos o sentimos en' +
+        'lugar de a otras personas, y el estilo formal, que usamos cuando nos dirigimos a alguien';
+    return <Topic name={nameRememberWhatYouWhereDoing} grammar={Grammar} description={description} examples={examples} />
 }
---
+++ src/components/Lessons/Lesson_8/RememberWhatYouWhereDoing/examples.js
@@ -0,0 +1,5 @@
+const examples = [
+    { example: { first: '運動した', highlight: '後で、', second: 'カフェでポカリスエットを飲んだ。' }, translation: 'Tras hacer ejercicio, me tomé un Pocari Sweat en la cagetería.' },
+    { example: { first: '家についた', highlight: '後で、', second: 'メールをチェックします。' }, translation: 'Al llegar a casa, revisaré el correo electrónico.' },
+];
+export default examples;
